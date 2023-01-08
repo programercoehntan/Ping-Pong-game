17f38c5e8c38891f213f0eb69f522178cba4a9f1
programercoehntan <119779047+programercoehntan@users.noreply.github.com>
2023-01-08 15:32:28 +0800

    final update

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from pygame import *
+from random import randint
 class GameSprite(sprite.Sprite):
     #class constructor
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-        #Call for the class (Sprite) constructor:
         super().__init__() #sprite.Sprite.__init__(self)
- 
-        #every sprite must store the image property
         self.image = transform.scale(image.load(player_image), (size_x, size_y))
         self.speed = player_speed
  
@@ -18,70 +16,89 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-class Paddle(GameSprite):
+#child class
+class Paddle (GameSprite):
     #method to control the sprite with arrow keys
     def update_right(self):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win_height - 80:
+        if keys[K_DOWN] and self.rect.y < win_height - 150:
             self.rect.y += self.speed
     def update_left(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < win_height - 80:
+        if keys[K_s] and self.rect.y < win_height - 150:
             self.rect.y += self.speed
 
-BLACK = (8, 8, 8)
-win_width = 600
-win_height = 500
+#interface
+BLACK = (13, 12, 12)
+win_width = 800
+win_height = 700
 window = display.set_mode((win_width, win_height))
-display.set_caption("Pong")
 window.fill(BLACK)
 
-paddle1 = "paddle1.png"
-paddle2 = "paddle2.png"
-sphere = "ball.png"
-paddle1 = Paddle(paddle1, 20, 200, 30, 150, 50)
-paddle2 = Paddle(paddle2, 520, 200, 30, 150, 50)
-ball = GameSprite(sphere, 330, 200, 50, 50, 1000) #   <-------------------
+
+#create sprites (paddle and balls)
+paddleA_img = "paddle1.png"
+paddleB_img = "paddle2.png"
+ball_img = "ball.png"
+
+paddleA = Paddle (paddleA_img, 20, 200, 30, 150, 10)
+paddleB = Paddle (paddleB_img, 750, 200, 30, 150, 10)
+ball = GameSprite(ball_img, 330, 200, 50, 50, 50)
+
+
+#game loop
 game = True
 finish = False
-FPS = 60
 clock = time.Clock()
-speed_x = 3    #   <-------------------
-speed_y = 3    #   <-------------------
+FPS = 60
+
+#fonts
+font.init()
+font = font.Font("pdark.ttf", 35)
+lose1 = font.render('PLAYER 1 LOST!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOST!', True, (180, 0, 0))
+
+speed_x = 7
+speed_y = 7
+
 while game:
     for e in event.get():
-        if e.type == QUIT:
+        if e.type ==QUIT:
             game = False
 
-        if finish != True:
-            window.fill (BLACK)
-            paddle1.update_left()
-            paddle2.update_right()
+    if finish != True:
+        window.fill (BLACK)
+        paddleA.update_left()
+        paddleB.update_right()
 
-            ball.rect.x += speed_x
-            ball.rect.y += speed_y
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
 
-            
-            if sprite.collide_rect(paddle1, ball) or sprite.collide_rect(paddle2, ball):
-                speed_x *= -1
-                speed_y *= 1
-            if ball.rect.y > win_height-50 or ball.rect.y < 0:
-                speed_y *= -1
+        if sprite.collide_rect(paddleA, ball) or sprite.collide_rect(paddleB, ball):
+            speed_x *= -1
+            speed_y *= 1
 
-            if ball.rect.x < 0:
-                finish = False
-                window.blit(lose1, (200,200))
+        #ball bounces when hit the up or bottom wall
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
 
-            if ball.rect.x > win_width:
-                finish = False
-                window.blit(lose2, 200,200)
+        #if ball flies behind this paddle, display loss condition for player left
+        if ball.rect.x < 0:
+            finish = False
+            window.blit(lose1, (200, 200))
 
-            paddle1.reset()
-            paddle2.reset()
-            ball.reset()
-        display.update()
-        clock.tick(FPS)
+        #if the ball flies behind this paddle, display loss condition for player right
+        if ball.rect.x > win_width:
+            finish = False
+            window.blit(lose2, (200, 200))
+
+        paddleA.reset()
+        paddleB.reset()
+        ball.reset()
+
+    display.update()
+    clock.tick(FPS)
